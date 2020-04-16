81e787df5ca62c6131b5e1d3b3d3a60097b3b79b
Crowdbotics <team@crowdbotics.com>
2020-04-16 02:17:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pass_the_plate_15896/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pass_the_plate_15896;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "pass_the_plate_15896";
+    }
+}
